9b644554c87451ad7265613430e4af3da6e7da90
Vadim Oleynik <vadim.oleynik@yalantis.com>
2014-06-12 11:26:08 +0300

    Remove unused import statements and commented callback.

--- app/src/main/java/com/yalantis/androidtechtest/app/MainActivity.java
+++ app/src/main/java/com/yalantis/androidtechtest/app/MainActivity.java
@@ -1,22 +1,13 @@
 package com.yalantis.androidtechtest.app;
 
-import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity{
@@ -84,13 +75,6 @@ public class MainActivity extends ActionBarActivity{
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
-
-        /*@Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }*/
     }
 
 }
